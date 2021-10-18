bc96b43377465061a31e88a1f8616bc0370f6c20
! SHAHEED <82898227+SHAHEEDx69@users.noreply.github.com>
2021-10-18 18:39:57 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##   Zphisher 	: 	Automated Phishing Tool
-##   Author 	: 	TAHMID RAYAT 
+##   Author 	: 	 SHAHEED
 ##   Version 	: 	2.2
 ##   Github 	: 	https://github.com/htr-tech
 
